a6b67049e1781cdc1b9dcd8afbc07fc4a749c836
somename <someemail>
2015-09-30 11:52:47 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -2,8 +2,8 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 public class SudokuVerifier {
 
-	public int verify() {
-		
+	public int verify(String candidateSolution) {
+		// returns 1 if the candidate solution is correct
 		return 0;
 	}
 }
