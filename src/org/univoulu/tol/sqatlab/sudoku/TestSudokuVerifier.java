f36d169a1edd43d858f61848c5cdd00faf4aee7f
somename <someemail>
2015-09-30 11:53:54 +0300

    besouro automatic message

---
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -0,0 +1,14 @@
+package org.univoulu.tol.sqatlab.sudoku;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSudokuVerifier {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
